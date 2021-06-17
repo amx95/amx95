c248749b9375fd4f7ba027cb9c16e8a6f4df9466
amhad.12345@mail.ru <amhad.12345@mail.ru>
2021-06-17 15:18:40 +0300

    8778u

---
+++ Source/chat2/chat2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "chat2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, chat2, "chat2" );
